1bbea6544c26302dcdda51618a9cd4eac444c2be
danny0ceanCA <danielgojeda.1@gmail.com>
2025-05-28 12:04:10 -0700

    Add user management endpoints

---
+++ app/api/routes/__init__.py
@@ -0,0 +1,12 @@
+from . import auth, students, jobs, match, reporting, bulk_upload_csv, admin_users, users
+
+__all__ = [
+    "auth",
+    "students",
+    "jobs",
+    "match",
+    "reporting",
+    "bulk_upload_csv",
+    "admin_users",
+    "users",
+]
