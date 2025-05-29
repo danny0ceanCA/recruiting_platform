c1d325a58b434a9332dd03342d1ab848ffcd27ae
daniel0ceanCA <danielgojeda.1@gmail.com>
2025-05-28 21:35:56 -0700

    Restartv0.5

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,13 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primaryDark: '#112240',
-        circuitBlue: '#073642',
-      },
-    },
-  },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: { extend: {} },
   plugins: [],
 }
